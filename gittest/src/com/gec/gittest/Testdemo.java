d9f7838909c028c96179b70671dd8249312b6aed
xieliwensmall <123@qq.com>
2020-08-14 18:46:54 +0800

    这是第一次提交

--- gittest/src/com/gec/gittest/Testdemo.java
+++ gittest/src/com/gec/gittest/Testdemo.java
@@ -4,4 +4,9 @@ public class Testdemo {
     public static void main(String[] args) {
         System.out.println("ssss");
     }
-}
+
+//    这是第二次提交
+    public void test(){
+        System.out.println("hhhhhh");
+
+    }}
